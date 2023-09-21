f7fa4fb49dbf74d9860c98d987411fdbaabe7dcb
Klaas Schoute <hello@student-techlife.com>
2023-09-21 14:42:16 +0200

    Send MQTT message on restart action

--- app/const.py
+++ app/const.py
@@ -1,6 +1,12 @@
 """Constants for the Stair Challenge app."""
 from typing import Final
 
+MQTT_SENSOR: Final = "sensor"
+
+# MQTT topics - Subscribed
 MQTT_TEST_TOPIC: Final = "test"
-MQTT_TRIGGER_TOPIC: Final = "sensor/+/trigger"
-MQTT_STATUS_TOPIC: Final = "sensor/+/status"
+MQTT_TRIGGER_TOPIC: Final = f"{MQTT_SENSOR}/+/trigger"
+MQTT_STATUS_TOPIC: Final = f"{MQTT_SENSOR}/+/status"
+
+# MQTT topics - Published
+MQTT_RESTART_ALL_TOPIC: Final = f"{MQTT_SENSOR}/restart_all"
