ab175c4b735fd87057065b372b4cc5e440248776
Klaas Schoute <hello@student-techlife.com>
2023-10-17 18:31:50 +0200

    Test again

--- tests/functional/test_cli.py
+++ tests/functional/test_cli.py
@@ -1,8 +1,6 @@
 """Test the CLI commands."""
 
-import pytest
 
-@pytest.mark.parametrize('mock_strip', 'cli_test_client')
 def test_initialize_database(cli_test_client, mock_strip):
     """Test the init_db command."""
     output = cli_test_client.invoke(args=["init_db"])
