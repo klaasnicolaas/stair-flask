8719c22c63998c139d0fb126c67ec3a88201b7e2
joostlek <joostlek@outlook.com>
2023-10-17 14:45:58 +0200

    I make it work v3

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,10 @@ from app.const import WORKOUTS
 
 @pytest.fixture(scope="module", autouse=True)
 def mock_strip() -> None:
-    with patch("app.led_controller.PixelStrip.begin"), patch("app.led_controller.PixelStrip.setPixelColor"), patch("app.led_controller.PixelStrip.show"):
+    with patch("app.led_controller.PixelStrip.begin"),\
+            patch("app.led_controller.PixelStrip.setPixelColor"),\
+            patch("app.led_controller.PixelStrip.numPixels"),\
+            patch("app.led_controller.PixelStrip.show"):
         yield
 
 @pytest.fixture(scope="module", autouse=True)
