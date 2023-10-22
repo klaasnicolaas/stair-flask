0b53f66c6377bc37c9ab66ff19758fae146272e9
Klaas Schoute <hello@student-techlife.com>
2023-10-22 20:36:55 +0200

    Update the test for dashboard view

--- tests/functional/test_dashboard.py
+++ tests/functional/test_dashboard.py
@@ -16,9 +16,9 @@ def test_dashboard_view(client: pytest.fixture) -> None:
     assert current_user.is_admin is True
     assert current_user.id == 1
 
-    response = client.get("/admin")
-    assert response.status_code == 308
-    assert response.request.path == "/admin"
+    response = client.get("/admin/")
+    assert response.status_code == 200
+    assert response.request.path == "/admin/"
 
 
 @pytest.mark.usefixtures("auth_client")
@@ -36,3 +36,37 @@ def test_sensors_view(client: pytest.fixture) -> None:
     response = client.get("/admin/sensors")
     assert response.status_code == 200
     assert response.request.path == "/admin/sensors"
+
+
+# @pytest.mark.usefixtures("auth_client")
+# def test_single_sensor_view(client: pytest.fixture) -> None:
+#     """Test the single sensor view as admin.
+
+#     Args:
+#     ----
+#         client: Test client for the Flask application.
+#     """
+#     assert current_user.is_authenticated is True
+#     assert current_user.is_admin is True
+#     assert current_user.id == 1
+
+#     response = client.get("/admin/sensors/1")
+#     assert response.status_code == 200
+#     assert response.request.path == "/admin/sensors/1"
+
+
+@pytest.mark.usefixtures("auth_client")
+def test_workouts_view(client: pytest.fixture) -> None:
+    """Test the workouts view as admin.
+
+    Args:
+    ----
+        client: Test client for the Flask application.
+    """
+    assert current_user.is_authenticated is True
+    assert current_user.is_admin is True
+    assert current_user.id == 1
+
+    response = client.get("/admin/workouts")
+    assert response.status_code == 200
+    assert response.request.path == "/admin/workouts"
