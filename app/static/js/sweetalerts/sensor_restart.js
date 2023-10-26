b0f80c35b1c35e2f7ba2b0d02a629574af05b653
Klaas Schoute <hello@student-techlife.com>
2023-10-26 14:49:18 +0200

    Add sensor by modal and delete them

--- app/static/js/sweetalerts/sensor_restart.js
+++ app/static/js/sweetalerts/sensor_restart.js
@@ -1,10 +1,13 @@
+// Show the SA for restarting a single sensor
 restart_sensor = function (event) {
   Swal.fire({
     title: 'Weet je het zeker?',
     text: 'Wanneer je deze actie uitvoert wordt de sensor opnieuw opgestart.',
     icon: 'warning',
     showCancelButton: true,
-    confirmButtonText: 'Restart',
+    confirmButtonColor: '#EF7D00',
+    cancelButtonText: "Annuleren",
+    confirmButtonText: 'Herstart',
   }).then((result) => {
     if (result.isConfirmed) {
       socket.emit('restart_sensors', event)
@@ -12,13 +15,16 @@ restart_sensor = function (event) {
   })
 }
 
+// Show the SA for restarting all sensors
 restart_all_sensors = function(event) {
     Swal.fire({
       title: 'Weet je het zeker?',
       text: 'Wanneer je deze actie uitvoert worden alle sensoren opnieuw opgestart.',
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonText: 'Restart',
+      confirmButtonColor: '#EF7D00',
+      cancelButtonText: "Annuleren",
+      confirmButtonText: 'Herstart',
     }).then((result) => {
       if (result.isConfirmed) {
         socket.emit('restart_sensors', event);
